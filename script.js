76eb9a488de8acc5cb62effe129bf3f920e9ac57
kunal21-collab <kunal200545@gmail.com>
2026-04-28 03:47:13 +0530

    fresh start

---
+++ script.js
@@ -0,0 +1,27 @@
+const boyInput = document.getElementById('boyName');
+const girlInput = document.getElementById('girlName');
+
+const result = document.getElementById("result");
+const button = document.getElementById('calculateBtn');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const boyName = boyInput.value.trim().toLowerCase();
+    const girlName = girlInput.value.trim().toLowerCase();
+
+    const boyLength = boyName.length;
+    const girlLength = girlName.length;
+
+    let ans;
+
+    if (
+        (boyName === "harsh" && girlName === "senior") ||
+        (boyName === "kartik" && girlName === "senior")
+    ) {
+        ans = 100;
+    } else {
+        ans = 70 + ((boyLength + girlLength) % 21);
+    }
+
+    result.textContent = `Love Score: ${ans}% ❤️`;
+});
